25fe90a338e0b482a8eaedc78ae3e65b86582813
Qwamob <juramedan@gmail.com>
2024-11-01 11:43:33 +0100

    Add project files.

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        //[Key] do not need to use key anotation as long as
+        //the property is named Id any other name needs the key anotation.
+        public int Id { get; set; }
+        [Required] // this is a data annotation that makes the title required / not nullamble property
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty; 
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+    }
+}
